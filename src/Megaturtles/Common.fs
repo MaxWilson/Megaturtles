d243e9afc019ff628c18c7f54388574334b046e1
Max Wilson <wilson.max@gmail.com>
2022-12-05 07:00:28 -0800

    Implement Forward and Turn instructions

--- src/Megaturtles/Common.fs
+++ src/Megaturtles/Common.fs
@@ -2,10 +2,12 @@
 module Common
 
 let notImpl v = failwith $"Not implemented. Email Max if you want this feature. {v}"
+let shouldntHappen v = failwith $"Bug! This shouldn't ever happen even though the type system doesn't prevent it--it should be prevented by runtime logic."
 let thunk v _ = v
 let thunk1 f x _ = f x
 let thunk2 f x y = f x y
 let flip f x y = f y x
+let rand = System.Random()
 
 module Tup2 =
     let create x y = x,y
@@ -14,3 +16,11 @@ module Tup2 =
     let map2 f (x,y) = x, f y
     let mapf f x y = (x,y) |> f
 let tup2 = Tup2.create
+module Array2 =
+    let replace m n newContent array =
+        array
+        |> Array.mapi (fun m' row ->
+            row |> Array.mapi (fun n' content ->
+                    if m = m' && n = n' then newContent else content
+                )
+            )
